36e5840874159f6dc741f90ede6d7da8e268c3db
ayorich <kayodele17@yahoo.com>
2022-06-27 08:12:01 +0100

    redis

---
+++ src/services/keys.ts
@@ -0,0 +1,5 @@
+export const pageCacheKey = (id: string) => 'pagecache#' + id;
+export const usersKey = (userId: string) => 'users#' + userId;
+
+export const sessionKey = (sessionId: string) => `sessions#${sessionId}`;
+export const itemsKey = (itemId: string) => `items#${itemId}`;
