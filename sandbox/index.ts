8fc2d73a51a4b529dce1e9727bf31ecfa6737963
ayorich <kayodele17@yahoo.com>
2022-07-04 06:34:27 +0100

    sets-sorted-set

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -2,15 +2,25 @@ import 'dotenv/config';
 import { client } from '../src/services/redis';
 
 const run = async () => {
-	await client.hSet('car', {
+	await client.hSet('car1', {
 		color: 'red',
-		year: 1950,
-		engine: { cylinders: 8 },
-		owner: null,
-		service: undefined
+		year: 1950
+	});
+	await client.hSet('car2', {
+		color: 'green',
+		year: 1955
+	});
+	await client.hSet('car3', {
+		color: 'blue',
+		year: 1960
 	});
 
-	const car = await client.hGetAll('car');
-	console.log(car);
+	const result = await Promise.all([
+		client.hGetAll('car1'),
+		client.hGetAll('car2'),
+		client.hGetAll('car3')
+	]);
+
+	console.log(result);
 };
 run();
--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,8 +1,20 @@
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 import { client } from '$services/redis';
-import { usersKey } from '$services/keys';
-export const getUserByUsername = async (username: string) => {};
+import { usersKey, usernamesUniqueKey, usernamesKey } from '$services/keys';
+export const getUserByUsername = async (username: string) => {
+	const decimalId = await client.zScore(usernamesKey(), username);
+
+	if (!decimalId) {
+		throw new Error('user does not exist');
+	}
+
+	const id = decimalId.toString(16);
+
+	const user = await client.hGetAll(usersKey(id));
+
+	return deserialize(id, user);
+};
 
 export const getUserById = async (id: string) => {
 	const user = await client.hGetAll(usersKey(id));
@@ -12,9 +24,21 @@ export const getUserById = async (id: string) => {
 
 export const createUser = async (attrs: CreateUserAttrs) => {
 	const id = genId();
+	//see if the username is already in the set of usernames
+	const exists = await client.sIsMember(usernamesUniqueKey(), attrs.username);
+	if (exists) {
+		throw new Error('username is taken');
+	}
 
-	await client.hSet(usersKey(id), serialize(attrs));
+	await client.hSet(usersKey(id), serialize(attrs)); //hash
 
+	await client.sAdd(usernamesUniqueKey(), attrs.username); //set
+
+	await client.zAdd(usernamesKey(), {
+		//sorted set
+		value: attrs.username,
+		score: parseInt(id, 16)
+	});
 	return id;
 };
 
